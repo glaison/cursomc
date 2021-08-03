0880e5aa0023e3b5b487c07b213c5d2a0b10944f
Glaison Alencar <glaisonc@gmail.com>
2021-08-03 06:47:22 -0300

    Testando a primeira classe de dominip - Categoria

---
+++ src/main/java/com/fac/cursomc/domain/Categoria.java
@@ -0,0 +1,70 @@
+package com.fac.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+// o serializble, serve para dizer a classe categoria que os objetos podem ser convertidos para uma sequencia de bytes
+// pra que serve ? para gravar em arquivo, para navegar na rede.
+public class Categoria implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String nome;
+	
+	
+	//construtor da classe - Instancia os objs sem jogar valores para os atributos.
+	public Categoria() {
+		
+	}
+
+
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	//hascode para comparação entre dois objetos - Padrão do java, colocar somente o id
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
